19bca5be6cd59d46097b07212c3150bff185855b
Kamil Chlebek <kamil.chlebek94@gmail.com>
2022-04-05 19:05:35 +0200

    Init react-native-ui-lib

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,17 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { RootTabScreenProps } from '../types'
+import { View, TextField, Text, Button } from 'react-native-ui-lib'
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View flex paddingH-25 paddwingT-120>
+      <Text blue50 text20>
+        Welcome
+      </Text>
+      <TextField text50 placeholder="username" grey10 />
+      <TextField text50 placeholder="password" secureTextEntry grey10 />
+      <View marginT-100 center>
+        <Button text70 white background-orange30 label="Login" />
+        <Button link text70 orange30 label="Sign Up" marginT-20 />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -29,4 +34,4 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-});
+})

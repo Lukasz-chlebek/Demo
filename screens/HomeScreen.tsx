8d515ce4b45bc0daa3266227e054eafe6f49081c
Kamil Chlebek <kamil.chlebek94@gmail.com>
2022-04-13 18:43:40 +0200

    Basic add deck modal

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,30 @@
 import { RootStackScreenProps } from '../types'
-import { Divider, Icon, Layout, MenuItem, OverflowMenu, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  Divider,
+  Icon,
+  Input,
+  Layout,
+  MenuItem,
+  Modal,
+  OverflowMenu,
+  Text,
+  TopNavigation,
+  TopNavigationAction,
+} from '@ui-kitten/components'
 import { DecksList } from '../components/DecksList'
 import { useState } from 'react'
+import { StyleSheet } from 'react-native'
 
 const MenuIcon = (props: any) => <Icon {...props} name="more-vertical" />
+const AddIcon = (props: any) => <Icon {...props} name="plus-outline" />
 
 export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>) {
   const [menuVisible, setMenuVisible] = useState(false)
+  const [addModalVisible, setAddModalVisible] = useState(false)
+  const [newDeckName, setNewDeckName] = useState('')
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible)
@@ -15,10 +33,14 @@ export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>)
   const renderMenuAction = () => <TopNavigationAction icon={MenuIcon} onPress={toggleMenu} />
 
   const renderRightActions = () => (
-    <OverflowMenu anchor={renderMenuAction} visible={menuVisible} onBackdropPress={toggleMenu}>
-      <MenuItem title="Eksport" />
-      <MenuItem title="Import" />
-    </OverflowMenu>
+    <>
+      <TopNavigationAction icon={AddIcon} onPress={() => setAddModalVisible(true)} />
+
+      <OverflowMenu anchor={renderMenuAction} visible={menuVisible} onBackdropPress={toggleMenu}>
+        <MenuItem title="Eksport" />
+        <MenuItem title="Import" />
+      </OverflowMenu>
+    </>
   )
 
   return (
@@ -26,8 +48,35 @@ export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>)
       <TopNavigation title="Fiszki" alignment="center" accessoryRight={renderRightActions} />
       <Divider />
       <Layout>
+        <Modal
+          visible={addModalVisible}
+          backdropStyle={styles.backdrop}
+          onBackdropPress={() => setAddModalVisible(false)}
+        >
+          <Card disabled={true}>
+            <Text>Dodaj talię kart</Text>
+            <Input
+              value={newDeckName}
+              placeholder="Podaj nazwę nowej talii"
+              onChangeText={(nextValue) => setNewDeckName(nextValue)}
+            />
+            <ButtonGroup>
+              <Button onPress={() => setAddModalVisible(false)} status="basic">
+                Anuluj
+              </Button>
+              <Button onPress={() => {}}>Zapisz</Button>
+            </ButtonGroup>
+          </Card>
+        </Modal>
+
         <DecksList />
       </Layout>
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  backdrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+})

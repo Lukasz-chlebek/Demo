806845d4dd3257a6523d6de2b593f80dff0d0490
Kamil Chlebek <kamil.chlebek94@gmail.com>
2022-04-16 16:54:08 +0200

    Add new deck

--- features/home/decks.service.ts
+++ features/home/decks.service.ts
@@ -1,29 +1,51 @@
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Deck } from './deck'
 
+// @TODO: @kamil sqlite
+const db = [
+  {
+    id: 'id1',
+    name: 'nazwa',
+    stats: {
+      new: 0,
+      review: 0,
+    },
+  },
+]
 export const decksApi = createApi({
   reducerPath: 'decksApi',
   baseQuery: fakeBaseQuery<unknown>(),
-
+  tagTypes: ['Decks'],
   endpoints: (builder) => ({
     getAll: builder.query<Deck[], void>({
+      providesTags: ['Decks'],
       async queryFn() {
-        // @TODO: @kamil sqlite
         return {
-          data: [
-            {
-              id: 'id1',
-              name: 'nazwa',
-              stats: {
-                new: 0,
-                review: 0,
-              },
-            },
-          ],
+          data: [...db],
+        }
+      },
+    }),
+    addDeck: builder.mutation<Deck, { name: string }>({
+      invalidatesTags: ['Decks'],
+      async queryFn(body: any) {
+        const deck = {
+          id: 'id1' + body.name,
+          name: body.name,
+          stats: {
+            new: 0,
+            review: 0,
+          },
+        }
+        db.push(deck)
+
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+
+        return {
+          data: deck,
         }
       },
     }),
   }),
 })
 
-export const { useGetAllQuery } = decksApi
+export const { useGetAllQuery, useAddDeckMutation } = decksApi
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -9,6 +9,7 @@ import {
   MenuItem,
   Modal,
   OverflowMenu,
+  Spinner,
   Text,
   TopNavigation,
   TopNavigationAction,
@@ -16,14 +17,106 @@ import {
 import { DecksList } from '../components/DecksList'
 import { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
+import { useAddDeckMutation } from '../features/home/decks.service'
 
 const MenuIcon = (props: any) => <Icon {...props} name="more-vertical" />
 const AddIcon = (props: any) => <Icon {...props} name="plus-outline" />
 
+const AddDeckModal = ({
+  visible,
+  onAddCancel,
+  onAddSuccess,
+}: {
+  visible: boolean
+  onAddCancel: () => void
+  onAddSuccess: () => void
+}) => {
+  const [newDeckName, setNewDeckName] = useState('')
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const [addDeck, { isLoading, isSuccess }] = useAddDeckMutation()
+
+  // @TODO: @kamil reload home list
+
+  const onSavePress = () => {
+    if (isLoading) {
+      return
+    }
+
+    setFormSubmitted(true)
+    if (newDeckName) {
+      addDeck({
+        name: newDeckName,
+      })
+        .unwrap()
+        .then(() => {
+          onAddSuccess()
+        })
+    }
+  }
+
+  const onBackDropPress = () => {
+    if (isLoading) {
+      return
+    }
+    onAddCancel()
+  }
+
+  const onCancelPress = () => {
+    if (isLoading) {
+      return
+    }
+    onAddCancel()
+  }
+
+  const LoadingIndicator = (props: any) => (
+    <View style={[props.style, modalStyles.indicator]}>
+      <Spinner />
+    </View>
+  )
+
+  return (
+    <Modal visible={visible} backdropStyle={modalStyles.backdrop} onBackdropPress={onBackDropPress}>
+      <Card disabled={true}>
+        <Text category="s1">Dodaj talię kart</Text>
+        <Input
+          status={formSubmitted && !newDeckName ? 'danger' : 'basic'}
+          style={modalStyles.input}
+          value={newDeckName}
+          placeholder="Podaj nazwę nowej talii"
+          onChangeText={(nextValue) => setNewDeckName(nextValue)}
+        />
+        <View style={modalStyles.actionContainer}>
+          {isLoading ? <LoadingIndicator /> : undefined}
+          <Button appearance="ghost" onPress={onCancelPress}>
+            Anuluj
+          </Button>
+          <Button onPress={onSavePress}>Zapisz</Button>
+        </View>
+      </Card>
+    </Modal>
+  )
+}
+
+const modalStyles = StyleSheet.create({
+  backdrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  input: {
+    marginVertical: 16,
+  },
+  actionContainer: {
+    flexDirection: 'row',
+    alignSelf: 'flex-end',
+  },
+  indicator: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
 export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>) {
   const [menuVisible, setMenuVisible] = useState(false)
   const [addModalVisible, setAddModalVisible] = useState(false)
-  const [newDeckName, setNewDeckName] = useState('')
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible)
@@ -47,43 +140,14 @@ export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>)
       <TopNavigation title="Fiszki" alignment="center" accessoryRight={renderRightActions} />
       <Divider />
       <Layout>
-        <Modal
+        <AddDeckModal
           visible={addModalVisible}
-          backdropStyle={styles.backdrop}
-          onBackdropPress={() => setAddModalVisible(false)}
-        >
-          <Card disabled={true}>
-            <Text category="s1">Dodaj talię kart</Text>
-            <Input
-              style={styles.input}
-              value={newDeckName}
-              placeholder="Podaj nazwę nowej talii"
-              onChangeText={(nextValue) => setNewDeckName(nextValue)}
-            />
-            <View style={styles.actionContainer}>
-              <Button appearance="ghost" onPress={() => setAddModalVisible(false)}>
-                Anuluj
-              </Button>
-              <Button onPress={() => {}}>Zapisz</Button>
-            </View>
-          </Card>
-        </Modal>
+          onAddCancel={() => setAddModalVisible(false)}
+          onAddSuccess={() => setAddModalVisible(false)}
+        ></AddDeckModal>
 
         <DecksList />
       </Layout>
     </>
   )
 }
-
-const styles = StyleSheet.create({
-  backdrop: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  input: {
-    marginVertical: 16,
-  },
-  actionContainer: {
-    flexDirection: 'row',
-    alignSelf: 'flex-end',
-  },
-})

54fff51fc06e9df69967a7a2b60ea16f249af862
Kamil Chlebek <kamil.chlebek94@gmail.com>
2022-04-19 18:33:39 +0200

    Search and scroll to result

--- screens/CardsListScreen.tsx
+++ screens/CardsListScreen.tsx
@@ -1,7 +1,8 @@
 import { RootStackScreenProps } from '../types'
-import { Divider, Icon, Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
+import { Button, Card, Divider, Icon, Input, Layout, Modal, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
 import { StyleSheet, View } from 'react-native'
 import BigList from 'react-native-big-list'
+import { useRef, useState } from 'react'
 
 const BackIcon = (props: any) => <Icon {...props} name="arrow-back" />
 const SearchIcon = (props: any) => <Icon {...props} name="search" />
@@ -13,7 +14,70 @@ const getItem = (index: any) => ({
 
 const DATA: any = new Array(500).fill(1).map((_, index) => getItem(index))
 
+const SearchModal = ({
+  visible,
+  onSearch,
+  onCancel,
+}: {
+  visible: boolean
+  onSearch: (query: string) => void
+  onCancel: () => void
+}) => {
+  const [query, setQuery] = useState('')
+
+  const onSearchPress = () => {
+    onSearch(query)
+  }
+
+  const onBackDropPress = () => {
+    onCancel()
+  }
+
+  const onCancelPress = () => {
+    onCancel()
+  }
+
+  return (
+    <Modal visible={visible} backdropStyle={modalStyles.backdrop} onBackdropPress={onBackDropPress}>
+      <Card disabled={true}>
+        <Text category="s1">Szukaj...</Text>
+        <Input
+          style={modalStyles.input}
+          value={query}
+          onChangeText={(nextValue) => setQuery(nextValue)}
+        />
+        <View style={modalStyles.actionContainer}>
+          <Button appearance="ghost" onPress={onCancelPress}>
+            Anuluj
+          </Button>
+          <Button onPress={onSearchPress}>Szukaj</Button>
+        </View>
+      </Card>
+    </Modal>
+  )
+}
+
+const modalStyles = StyleSheet.create({
+  backdrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  input: {
+    marginVertical: 16,
+  },
+  actionContainer: {
+    flexDirection: 'row',
+    alignSelf: 'flex-end',
+  },
+  indicator: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
 export default function CardsListScreen({ navigation }: RootStackScreenProps<'CardsList'>) {
+  const [searchModalVisible, setSearchModalVisible] = useState(false)
+  const list = useRef<any>(null)
+
   const renderBackAction = () => (
     <TopNavigationAction icon={BackIcon} onPress={() => navigation.replace('Home')} />
   )
@@ -21,6 +85,7 @@ export default function CardsListScreen({ navigation }: RootStackScreenProps<'Ca
     <TopNavigationAction
       icon={SearchIcon}
       onPress={() => {
+        setSearchModalVisible(true)
         // @TODO: @kamil display modal with input
         // @TODO: @kamil scroll to desired card
       }}
@@ -35,15 +100,24 @@ export default function CardsListScreen({ navigation }: RootStackScreenProps<'Ca
     )
   }
 
-  const MyExample = ({ data }: any) => {
-    const renderItem = ({ item, index }: any) => {
-      return <Item item={item} />
-    }
-    return <BigList data={data} renderItem={renderItem} itemHeight={150} />
+  const renderItem = ({ item, index }: any) => {
+    return <Item item={item} />
   }
 
   return (
     <>
+      <SearchModal
+        visible={searchModalVisible}
+        onSearch={(query) => {
+          setSearchModalVisible(false)
+
+          const index = DATA.findIndex((i: any) => i.title.includes(query))
+          if (index) {
+            list.current.scrollToIndex({ index })
+          }
+        }}
+        onCancel={() => setSearchModalVisible(false)}
+      ></SearchModal>
       <TopNavigation
         title="Fiszki"
         alignment="center"
@@ -53,7 +127,13 @@ export default function CardsListScreen({ navigation }: RootStackScreenProps<'Ca
       <Divider />
       <Layout style={{ flex: 1 }}>
         <View style={{ flex: 1 }}>
-          <MyExample data={DATA} keyExtractor={(item: any) => item.id} />
+          <BigList
+            ref={list}
+            data={DATA}
+            renderItem={renderItem}
+            itemHeight={150}
+            keyExtractor={(item: any) => item.id}
+          />
         </View>
       </Layout>
     </>

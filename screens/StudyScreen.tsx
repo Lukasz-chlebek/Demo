b9b4d828e82cf351025392ec1e63854cd91c6653
Kamil Chlebek <kamil.chlebek94@gmail.com>
2022-04-24 18:34:55 +0200

    StudyScreen layout

--- screens/StudyScreen.tsx
+++ screens/StudyScreen.tsx
@@ -1,7 +1,19 @@
 import { RootStackScreenProps } from '../types'
-import { Divider, Icon, Layout, MenuItem, OverflowMenu, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
-import { StyleSheet } from 'react-native'
-import { useState } from 'react'
+import {
+  Button,
+  Divider,
+  Icon,
+  Layout,
+  MenuItem,
+  OverflowMenu,
+  StyleService,
+  Text,
+  TopNavigation,
+  TopNavigationAction,
+  useStyleSheet,
+} from '@ui-kitten/components'
+import { ScrollView, View } from 'react-native'
+import React, { useState } from 'react'
 
 const BackIcon = (props: any) => <Icon {...props} name="arrow-back" />
 const MenuIcon = (props: any) => <Icon {...props} name="more-vertical" />
@@ -28,6 +40,9 @@ export default function StudyScreen({ navigation }: RootStackScreenProps<'Study'
     </>
   )
 
+  const [backVisible, setBackVisible] = useState(false)
+  const styles = useStyleSheet(themedStyles)
+
   return (
     <>
       <TopNavigation
@@ -37,9 +52,75 @@ export default function StudyScreen({ navigation }: RootStackScreenProps<'Study'
         accessoryRight={renderRightActions}
       />
       <Divider />
-      <Layout></Layout>
+      <Layout style={{ flex: 1 }}>
+        <ScrollView>
+          <Text style={styles.front}>Front</Text>
+
+          {backVisible ? <Text style={styles.back}>Back</Text> : ''}
+        </ScrollView>
+
+        {backVisible ? (
+          <>
+            <View style={styles.buttonContainer}>
+              <Button style={[styles.button, styles.dontKnow]} onPress={() => {}}>
+                Nie wiem
+              </Button>
+              <Button style={[styles.button, styles.difficult]} onPress={() => {}}>
+                Trudne
+              </Button>
+              <Button style={[styles.button, styles.know]} onPress={() => {}}>
+                Wiem
+              </Button>
+            </View>
+          </>
+        ) : (
+          <Button onPress={() => setBackVisible(true)} style={styles.showBack}>
+            Pokaż odpowiedź
+          </Button>
+        )}
+      </Layout>
     </>
   )
 }
 
-const styles = StyleSheet.create({})
+const themedStyles = StyleService.create({
+  front: {
+    fontSize: 17,
+    padding: 25,
+    textAlign: 'center',
+  },
+  back: {
+    fontSize: 17,
+    borderTopWidth: 1,
+    borderTopColor: 'color-basic-default-border',
+    padding: 25,
+    textAlign: 'center',
+  },
+  showBack: {
+    margin: 15,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 5,
+    marginVertical: 15,
+  },
+  button: {
+    flex: 1,
+    marginHorizontal: 10,
+  },
+  dontKnow: {
+    backgroundColor: '#969696',
+    borderColor: '#969696',
+    color: 'white',
+  },
+  difficult: {
+    backgroundColor: '#FF9900',
+    borderColor: '#FF9900',
+    color: 'white',
+  },
+  know: {
+    backgroundColor: '#3CA617',
+    borderColor: '#3CA617',
+    color: 'white',
+  },
+})
